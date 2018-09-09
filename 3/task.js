e2275bac6b6755c0b33b6b5f0b31446914aa532d
AntonVoronezh <pstut@mail.ru>
2018-09-09 19:36:25 +0300

    3 #4

---
+++ 3/task.js
@@ -0,0 +1,8 @@
+const styles = ['Джаз', 'Блюз'];
+
+styles.push('Рок-н-Ролл');
+styles[styles.length - 2] = 'Классика';
+console.log(styles.shift());
+styles.unshift('Рэп', 'Регги');
+
+console.log(styles);
