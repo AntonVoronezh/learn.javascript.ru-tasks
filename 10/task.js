008bb4536385e23eed5816eb11d1f0e45acaa8fa
AntonVoronezh <pstut@mail.ru>
2018-09-16 21:23:56 +0300

    10 #5

--- 10/task.js
+++ 10/task.js
@@ -1,1 +1,12 @@
+const makeArr = arg => arg.split('-');
+
+const camelize = (str) => {
+  const arrFromStr = makeArr(str);
+  let out = '';
+  for (let i = 1; i < arrFromStr.length; i += 1) {
+    out += arrFromStr[i][0].toUpperCase() + arrFromStr[i].slice(1);
+    console.log(`${out} - ${i}`);
+  }
+  return arrFromStr[0] + out;
+};
 
